bedc209d4f45b6cfe2b6b728d747c112a3829ab6
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-09-26 19:24:22 +0530

    DB connection string setup

--- Backend/app.js
+++ Backend/app.js
@@ -1,31 +1,47 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-require('dotenv/config');
-
-const api = process.env.API_URL;
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 
 //middleware
 app.use(express.json());
+app.use(morgan("tiny"));
+
+require("dotenv/config");
+
+const api = process.env.API_URL;
 
 // http://localhost:3000/api/v1/products
-app.get(`${api}/products`, (req, res) =>{
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some_url',
-    }
-    res.send(product);
-})
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some_url",
+  };
+  res.send(product);
+});
 
-app.post(`${api}/products`, (req, res) =>{
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
-})
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
+});
 
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    //useNewUrlParser: true,
+    //useUnifiedTopology: true,
+    //dbName: "e-commerce-db",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
-    console.log(api);
-    console.log('Server is running http://localhost:3000');
-})
+  console.log(api);
+  console.log("Server is running http://localhost:3000");
+});
