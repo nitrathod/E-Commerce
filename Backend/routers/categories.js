f82784430014b1f45b5fd6f75254f8a65a7a7e72
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-09-27 16:47:03 +0530

    Added category API with add and update methods

--- Backend/routers/categories.js
+++ Backend/routers/categories.js
@@ -2,15 +2,27 @@ const { Category } = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
+//Fetch all categories list details
 router.get(`/`, async (req, res) =>{
     const categoryList = await Category.find();
 
     if(!categoryList){
         res.status(500).json({success: false})
     };
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 });
 
+//Fetch category details using ID
+router.get('/:id', async (req, res) => {
+    const category = await Category.findById(req.params.id);
+
+    if(!category){
+        return res.status(500).json({message: 'The category with the given ID was not found.'});
+    }
+    res.status(200).send(category);
+});
+
+//Add category details
 router.post('/', async (req, res) => {
     let category = new Category({
         name: req.body.name,
@@ -25,6 +37,28 @@ router.post('/', async (req, res) => {
     res.send(category);
 });
 
+//Update Category details
+router.put('/:id', async(req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color,
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!category)
+        return res.status(404).send('the category cannot be created!');
+    
+    res.send(category);
+
+});
+
+//Delete category details
 router.delete('/:id', (req, res) => {
     Category.findByIdAndRemove(req.params.id).then(category => {
         if(category){
