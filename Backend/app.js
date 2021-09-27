0651827ed0e75c4152f0d106077c1ea53335a4cf
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-09-27 11:04:22 +0530

    Product Schema updated

--- Backend/app.js
+++ Backend/app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 require("dotenv/config");
+
+app.use(cors());
+app.options('*', cors());
 
 //middleware
 app.use(express.json());
