8882b9d80d901ff4761a795da11a5b1e058c1fc5
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-09-28 22:30:50 +0530

    Updated users API with login authenticated API with JWT

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -2,6 +2,7 @@ const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //Fetch all user details
 router.get(`/`, async (req, res) => {
@@ -97,6 +98,34 @@ router.delete('/:id', (req, res) => {
     }).catch(err => {
         return res.status(400).json({success: false, error: err});
     })
+});
+
+//Authenticating user
+router.post('/login', async (req, res) => {
+    const user = await User.findOne({email: req.body.email});
+
+    if(!user){
+        return res.status(400).send('The user not found');
+    }
+
+    if(user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+
+        //authenticating user with jwt
+        const token = jwt.sign(
+            {
+                userID: user.id
+            },
+            'secret',
+            {
+                //token expire
+                expiresIn: '1d'
+            }
+        );
+
+        res.status(200).send({user: user.email, token: token})
+    } else {
+        res.status(400).send('Incorrect password.');
+    }
 });
 
 module.exports = router;
