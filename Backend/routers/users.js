39ab246eb5168e4e9e58002591d93f78bfcda886
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-09-28 21:50:45 +0530

    Updated users API with get user by ID and add user methods

--- Backend/routers/users.js
+++ Backend/routers/users.js
@@ -1,9 +1,11 @@
 const {User} = require('../models/user');
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 
+//Fetch all user details
 router.get(`/`, async (req, res) => {
-    const userList = await User.find();
+    const userList = await User.find().select('-passwordHash');
 
     if(!userList){
         res.status(500).json({success: false});
@@ -11,5 +13,38 @@ router.get(`/`, async (req, res) => {
 
     res.send(userList);
 });
+
+//Fetch user details using ID
+router.get('/:id', async (req, res) => {
+    const user = await User.findById(req.params.id).select('-passwordHash');
+
+    if(!user){
+        return res.status(500).json({message: 'The user with the given ID was not found.'});
+    }
+    res.status(200).send(user);
+});
+
+//Add user details to DB
+router.post('/', async (req, res) => {
+    let user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        color: req.body.color,
+        passwordHash: bcrypt.hashSync(req.body.password, 07),
+        phone: req.body.phone,
+        isAdmin: req.body.isAdmin,
+        apartment: req.body.apartment,
+        zip: req.body.zip,
+        city: req.body.city,
+        country: req.body.country
+    })
+    user = await user.save();
+
+    if(!user)
+        return res.status(404).send('the user cannot be registered!');
+    
+    res.send(user);
+});
+
 
 module.exports = router;
