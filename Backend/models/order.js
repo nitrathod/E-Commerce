e5cb8ace4161a52d536d3373e3e4770a82024db0
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-09-30 21:54:32 +0530

    orders schema created

--- Backend/models/order.js
+++ Backend/models/order.js
@@ -1,7 +1,60 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
-    
-})
+  orderItems: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "orderItem",
+      required: true,
+    },
+  ],
+  shippingAddress1: {
+    type: String,
+    required: true,
+  },
+  shippingAddress2: {
+    type: String,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    default: "pending",
+  },
+  totalPrice: {
+    type: Number,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  dateOrdered: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-exports.Order = mongoose.model('Order', orderSchema);
+orderSchema.virtual("id").get(function () {
+  return this._id.toHexString();
+});
+
+orderSchema.set("toJSON", {
+  virtuals: true,
+});
+
+exports.Order = mongoose.model("Order", orderSchema);
