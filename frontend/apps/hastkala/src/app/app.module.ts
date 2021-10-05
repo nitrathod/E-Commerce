fc500f6d40bb27cfb0320b5ef2cdd26d68c99659
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-10-05 23:01:51 +0530

    shared component added and modification of routing

--- frontend/apps/hastkala/src/app/app.module.ts
+++ frontend/apps/hastkala/src/app/app.module.ts
@@ -1,13 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
+const routes: Routes = [
+    {
+        path: '',
+        component: HomePageComponent
+    },
+    {
+        path: 'products',
+        component: ProductListComponent
+    }
+];
 @NgModule({
-  declarations: [AppComponent, HomePageComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent, HomePageComponent, ProductListComponent, FooterComponent, HeaderComponent],
+    imports: [BrowserModule, RouterModule.forRoot(routes)],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
