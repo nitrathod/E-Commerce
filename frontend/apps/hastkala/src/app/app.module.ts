09b874ce4ad854037bbf8f249c8152f7c2c9beaa
nitrathod <55508648+nitrathod@users.noreply.github.com>
2021-10-05 22:38:04 +0530

    Initial Commit

--- frontend/apps/hastkala/src/app/app.module.ts
+++ frontend/apps/hastkala/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomePageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
